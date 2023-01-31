9dfeefa6ce6c1cb5b019e05ebd88a4468e6438a7
blaine-t <github@bathost.net>
2023-01-30 18:46:10 -0600

    Licensing update

--- piMI/main.py
+++ piMI/main.py
@@ -4,10 +4,33 @@ from uasyncio import create_task, sleep, run
 from serial import listen
 # Allow for connection to wireless
 from wireless import connectWireless
-# TODO: Temp for debugging
+# Allow for GPIO access
 from gpio import shortPress, longPress, resetPress
+# Allow for display access
+from display import test
 
-# Original code by Florin Dragan licensed under the MIT License: https://gitlab.com/florindragan/raspberry_pico_w_websocket/-/blob/main/LICENSE
+# Original code for web socket server by Florin Dragan licensed under the MIT License: https://gitlab.com/florindragan/raspberry_pico_w_websocket/-/blob/main/LICENSE
+# MIT License
+# 
+# Copyright (c) 2023 Florin Dragan
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 from ws_connection import ClientClosedError
 from ws_server import WebSocketServer, WebSocketClient
 
@@ -56,9 +79,9 @@ server.start()
 async def main():
     # "Setup"
     create_task(listen(server))
+    create_task(test())
     # "Loop"
     while True:
-        # Display here eventually
         server.parse_all()
         await sleep(1)
 
